6a7435b67cd14db5c9d723b02cb71e39ea3aee29
Jamie Hirst <jamie.hirst10@gmail.com>
2021-11-27 16:20:20 +0000

    updated organisation of computer guess code to be clearer

--- computer.py
+++ computer.py
@@ -42,91 +42,103 @@ class Computer_Player:
                 computer_ship_instance.place_ship(ship_location, dict_name)
 
     def computer_guess(self, player_ship_instance, player_board_instance, computer_instance):
-        if not self.has_hit:  #make into function? This is the default       
-            comp_guess = (random.randint(0, 9), random.randint(0, 9))
-            if comp_guess in self.guesses:
-                self.computer_guess(player_ship_instance, player_board_instance, computer_instance)
-            else:
-                self.guesses.append(comp_guess)
-                output_guess = Coordinate.index_to_user(comp_guess)
-                if player_ship_instance.does_this_hit(comp_guess):
-                    self.last_rand_guess_loc = comp_guess
-                    print(f'The computer has hit your ship at {output_guess}')
-                    player_board_instance.update_board(comp_guess, 'X')
-                    sunk = player_ship_instance.has_ship_sunk(computer_instance)
-                    if sunk:
-                        print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
-                    else:
-                        self.has_hit = True          
-                else:
-                    print(f'The computer missed your ships shooting at {output_guess}')
-                    player_board_instance.update_board(comp_guess, 'O')
-                    self.has_hit = False
-        elif self.has_hit: #make into function? Just after making random hit
+        if not self.has_hit:      
+            self.rand_comp_guess(player_ship_instance, player_board_instance, computer_instance)
+        elif self.has_hit:
             if not self.checking_adjacent:
                 if not self.adjacent_hit:
-                    self.adjacent_coords = Coordinate.all_adjacent(self.last_rand_guess_loc, 10)#make possible adjacent coords on board
-                    avaliable_adjacent_coords = [x for x in self.adjacent_coords if x not in self.guesses]#filter out guessed adjacent
-                    comp_guess = random.choice(avaliable_adjacent_coords)
-                    self.guesses.append(comp_guess)
-                    output_guess = Coordinate.index_to_user(comp_guess)
-                    if player_ship_instance.does_this_hit(comp_guess):#check if rand guess from adjacent coords hits
-                        print(f'The computer has hit your ship at {output_guess}')
-                        player_board_instance.update_board(comp_guess, 'X')
-                        sunk = player_ship_instance.has_ship_sunk(computer_instance)
-                        if sunk:  #if it sinks it resets has_hit back to making random guesses
-                            print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
-                            self.has_hit = False
-                        else:
-                            self.adjacent_hit_loc = comp_guess
-                            self.adjacent_hit = True
-                    else:
-                        print(f'The computer missed your ships shooting at {output_guess}')
-                        player_board_instance.update_board(comp_guess, 'O')
-                        self.checking_adjacent = True
+                    self.comp_find_adj_hit_guess(player_ship_instance, player_board_instance, computer_instance)
                 elif self.adjacent_hit:
-                    orientation = Coordinate.find_orientation(self.last_rand_guess_loc, self.adjacent_hit_loc)
-                    coords1 = Coordinate.dir_adjacent(orientation, self.last_rand_guess_loc, 10)
-                    coords2 = Coordinate.dir_adjacent(orientation, self.adjacent_hit_loc, 10)
-                    adj_coords = [x for x in coords1 + coords2 if x not in self.guesses]
-                    if adj_coords:
-                        comp_guess= random.choice(adj_coords)
-                        self.guesses.append(comp_guess)
-                        output_guess = Coordinate.index_to_user(comp_guess)
-                        if player_ship_instance.does_this_hit(comp_guess):#check if rand guess from adjacent coords hits
-                            print(f'The computer has hit your ship at {output_guess}')
-                            player_board_instance.update_board(comp_guess, 'X')
-                            sunk = player_ship_instance.has_ship_sunk(computer_instance)
-                            if sunk:  #if it sinks it resets has_hit back to making random guesses
-                                print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
-                                self.has_hit = False
-                                self.adjacent_hit = False
-                            else:
-                                self.adjacent_hit_loc = comp_guess
-                        else:
-                            print(f'The computer missed your ships shooting at {output_guess}')
-                            player_board_instance.update_board(comp_guess, 'O')
-            elif self.checking_adjacent: #make into function?
-                avaliable_adjacent_coords = [x for x in self.adjacent_coords if x not in self.guesses]
-                if avaliable_adjacent_coords:
-                    comp_guess = random.choice(avaliable_adjacent_coords)
-                    self.guesses.append(comp_guess)
-                    output_guess = Coordinate.index_to_user(comp_guess)
-                    if player_ship_instance.does_this_hit(comp_guess):
-                        print(f'The computer has hit your ship at {output_guess}')
-                        player_board_instance.update_board(comp_guess, 'X')
-                        sunk = player_ship_instance.has_ship_sunk(computer_instance)
-                        if sunk:
-                            print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
-                            self.has_hit = False
-                            self.checking_adjacent = False
-                        else:
-                            self.adjacent_hit = True
-                            self.checking_adjacent = False
-                    else:
-                        print(f'The computer missed your ships shooting at {output_guess}')
-                        player_board_instance.update_board(comp_guess, 'O')
+                    self.comp_found_adj_hit_guess(player_ship_instance, player_board_instance, computer_instance)
+            elif self.checking_adjacent:
+                self.comp_get_adj_guess(player_ship_instance, player_board_instance, computer_instance)
+
+    def rand_comp_guess(self, player_ship_instance, player_board_instance, computer_instance):
+        comp_guess = (random.randint(0, 9), random.randint(0, 9))
+        if comp_guess in self.guesses:
+            self.computer_guess(player_ship_instance, player_board_instance, computer_instance)
+        else:
+            self.guesses.append(comp_guess)
+            output_guess = Coordinate.index_to_user(comp_guess)
+            if player_ship_instance.does_this_hit(comp_guess):
+                self.last_rand_guess_loc = comp_guess
+                print(f'The computer has hit your ship at {output_guess}')
+                player_board_instance.update_board(comp_guess, 'X')
+                sunk = player_ship_instance.has_ship_sunk(computer_instance)
+                if sunk:
+                    print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
                 else:
+                    self.has_hit = True          
+            else:
+                print(f'The computer missed your ships shooting at {output_guess}')
+                player_board_instance.update_board(comp_guess, 'O')
+                self.has_hit = False
+
+    def comp_find_adj_hit_guess(self, player_ship_instance, player_board_instance, computer_instance):
+        self.adjacent_coords = Coordinate.all_adjacent(self.last_rand_guess_loc, 10)#make possible adjacent coords on board
+        avaliable_adjacent_coords = [x for x in self.adjacent_coords if x not in self.guesses]#filter out guessed adjacent
+        comp_guess = random.choice(avaliable_adjacent_coords)
+        self.guesses.append(comp_guess)
+        output_guess = Coordinate.index_to_user(comp_guess)
+        if player_ship_instance.does_this_hit(comp_guess):#check if rand guess from adjacent coords hits
+            print(f'The computer has hit your ship at {output_guess}')
+            player_board_instance.update_board(comp_guess, 'X')
+            sunk = player_ship_instance.has_ship_sunk(computer_instance)
+            if sunk:  #if it sinks it resets has_hit back to making random guesses
+                print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
+                self.has_hit = False
+            else:
+                self.adjacent_hit_loc = comp_guess
+                self.adjacent_hit = True
+        else:
+            print(f'The computer missed your ships shooting at {output_guess}')
+            player_board_instance.update_board(comp_guess, 'O')
+            self.checking_adjacent = True
+
+    def comp_found_adj_hit_guess(self, player_ship_instance, player_board_instance, computer_instance):
+        orientation = Coordinate.find_orientation(self.last_rand_guess_loc, self.adjacent_hit_loc)
+        coords1 = Coordinate.dir_adjacent(orientation, self.last_rand_guess_loc, 10)
+        coords2 = Coordinate.dir_adjacent(orientation, self.adjacent_hit_loc, 10)
+        adj_coords = [x for x in coords1 + coords2 if x not in self.guesses]
+        if adj_coords:
+            comp_guess= random.choice(adj_coords)
+            self.guesses.append(comp_guess)
+            output_guess = Coordinate.index_to_user(comp_guess)
+            if player_ship_instance.does_this_hit(comp_guess):#check if rand guess from adjacent coords hits
+                print(f'The computer has hit your ship at {output_guess}')
+                player_board_instance.update_board(comp_guess, 'X')
+                sunk = player_ship_instance.has_ship_sunk(computer_instance)
+                if sunk:  #if it sinks it resets has_hit back to making random guesses
+                    print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
+                    self.has_hit = False
+                    self.adjacent_hit = False
+                else:
+                    self.adjacent_hit_loc = comp_guess
+            else:
+                print(f'The computer missed your ships shooting at {output_guess}')
+                player_board_instance.update_board(comp_guess, 'O')
+
+    def comp_get_adj_guess(self, player_ship_instance, player_board_instance, computer_instance):
+        avaliable_adjacent_coords = [x for x in self.adjacent_coords if x not in self.guesses]
+        if avaliable_adjacent_coords:
+            comp_guess = random.choice(avaliable_adjacent_coords)
+            self.guesses.append(comp_guess)
+            output_guess = Coordinate.index_to_user(comp_guess)
+            if player_ship_instance.does_this_hit(comp_guess):
+                print(f'The computer has hit your ship at {output_guess}')
+                player_board_instance.update_board(comp_guess, 'X')
+                sunk = player_ship_instance.has_ship_sunk(computer_instance)
+                if sunk:
+                    print(f'The computer sunk your {player_ship_instance.sunk_ship_names[-1]}!')
                     self.has_hit = False
                     self.checking_adjacent = False
-                    self.computer_guess(player_ship_instance, player_board_instance, computer_instance)
+                else:
+                    self.adjacent_hit = True
+                    self.checking_adjacent = False
+            else:
+                print(f'The computer missed your ships shooting at {output_guess}')
+                player_board_instance.update_board(comp_guess, 'O')
+        else:
+            self.has_hit = False
+            self.checking_adjacent = False
+            self.computer_guess(player_ship_instance, player_board_instance, computer_instance)

0252906091a7e70a1819b6e20bd0592a94e6f06d
Jamie Hirst <jamie.hirst10@gmail.com>
2021-12-01 20:27:53 +0000

    changed has_ship_sunk to fix issues with recognising newly sunk ships

--- ship.py
+++ ship.py
@@ -4,13 +4,9 @@ class Ships:
         self.locations = {}
         self.hit_count = 0
         self.sunk_ships = []
-        self.sunk_ship_names = []
 
     def check_all_locs(self, locs):
-        if any(x in locs for y in self.locations.values() for x in y):
-            return True
-        else:
-            return False
+        return any(x in locs for y in self.locations.values() for x in y)
 
     def check_start_loc(self, index):
         if any(x for x in self.locations.values() if index in x):
@@ -27,16 +23,6 @@ class Ships:
     def has_ship_sunk(self, other_player_instance):
         for x in self.locations.items():
             if all(elem in other_player_instance.guesses for elem in x[1]):
-                if x[1] not in self.sunk_ships:
-                    self.sunk_ships.append(x[1])
-                    xsplit = (x[0]).split(' ', 1)
-                    self.sunk_ship_names.append(xsplit[0])
-                    return True
-                else:
-                    return False
-
-    #check on whether any ships have been sunk by a computer or player hit
-    #needs to be able to check whether all a ships locations have been hit
-    #maybe use guess list and when there is a new hit check against locations
-    #need to return key for values to declare ship sunk
-    #needs to be a check on which ships are sunk so they cannot be sunk by this again
+                if x not in self.sunk_ships:
+                    self.sunk_ships.append(x)
+                    return x

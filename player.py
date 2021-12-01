07f86b3da25a358048581822d40fa99b9b5910ac
Jamie Hirst <jamie.hirst10@gmail.com>
2021-12-01 20:22:08 +0000

    reduced nesting and duplication of ship input and player guess

--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 from coordinate import Coordinate
 
 
-class Human_Player:
+class HumanPlayer:
 
     def __init__(self):
         self.guesses = []
@@ -41,95 +41,59 @@ class Human_Player:
                 print('Sorry that is not a valid coordinate, it must be a letter from A-J and a number from 1-10, eg A1')
         except IndexError:
             print('Sorry that is not a valid coordinate, it must be a letter from A-J and a number from 1-10, eg A1')
+
+    def ship_orientation(self, format_coord, orientation, ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name):
+        ship_location = orientation(format_coord, ship_size, 10)
+        if player_ship_instance.check_all_locs(ship_location) or len(ship_location) != ship_size: 
+            print('Sorry either one of these coordinates has been taken or one of these coordinates does not exist on the board')
+            new_start_coor_option = input('Enter 1 if you would like to try a different orientation or 2 if you would like to change your starting coordinate   ')
+            if new_start_coor_option == '1':
+                return True
+            else:
+                self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
+        else:
+            player_ship_instance.place_ship(ship_location, dict_name)
+            print(player_ship_instance.locations.items())
+            player_board.player_set_ship(ship_location, ship_letter)
+            return False
         
     def ship_input(self, ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name):
         coord = input(f'Enter a coordinate for your{ship_number} {ship_type}   ')
         format_coord = self.format_input(coord)
         if format_coord == None:
+            print('Sorry thats not a valid coordinate')
             self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
-        else:
-            if player_ship_instance.check_start_loc(format_coord):
-                print('Sorry this coordinate is already occupied!')
-                self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)               
-            else:
-                while True:#use book database menu as example to stream line this code, could have menu function that returns value that then calls the right orientation?
-                    orientation = input('''From your coordinate what orientation would you like to place your ship on?\nenter 'up', 'down', 'left' or 'right':   ''')
-                    if orientation.lower() == 'up':
-                        ship_location = Coordinate.coords_up(format_coord, ship_size, 10)
-                        if player_ship_instance.check_all_locs(ship_location) or len(ship_location) != ship_size: 
-                            print('Sorry either one of these coordinates has been taken or one of these coordinates does not exist on the board')
-                            new_start_coor_option = input('Enter 1 if you would like to try a different orientation or 2 if you would like to change your starting coordinate   ')
-                            if new_start_coor_option == '1':
-                                pass
-                            else:
-                                self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
-                        else:
-                            player_ship_instance.place_ship(ship_location, dict_name)
-                            print(player_ship_instance.locations.items())
-                            player_board.player_set_ship(ship_location, ship_letter)
-                            break
-                    elif orientation.lower() == 'down':
-                        ship_location = Coordinate.coords_down(format_coord, ship_size, 10)
-                        if player_ship_instance.check_all_locs(ship_location) or len(ship_location) != ship_size: 
-                            print('Sorry either one of these coordinates has been taken or one of these coordinates does not exist on the board')
-                            new_start_coor_option = input('Enter 1 if you would like to try a different orientation or 2 if you would like to change your starting coordinate   ')
-                            if new_start_coor_option == '1':
-                                pass
-                            else:
-                                self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
-                        else:
-                            player_ship_instance.place_ship(ship_location, dict_name)
-                            print(player_ship_instance.locations.items())
-                            player_board.player_set_ship(ship_location, ship_letter)
-                            break
-                    elif orientation.lower() == 'left':
-                        ship_location = Coordinate.coords_left(format_coord, ship_size, 10)
-                        if player_ship_instance.check_all_locs(ship_location) or len(ship_location) != ship_size: 
-                            print('Sorry either one of these coordinates has been taken or one of these coordinates does not exist on the board')
-                            new_start_coor_option = input('Enter 1 if you would like to try a different orientation or 2 if you would like to change your starting coordinate   ')
-                            if new_start_coor_option == '1':
-                                pass
-                            else:
-                                self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
-                        else:
-                            player_ship_instance.place_ship(ship_location, dict_name)
-                            print(player_ship_instance.locations.items())
-                            player_board.player_set_ship(ship_location, ship_letter)
-                            break
-                    elif orientation.lower() == 'right':
-                        ship_location = Coordinate.coords_right(format_coord, ship_size, 10)
-                        if player_ship_instance.check_all_locs(ship_location) or len(ship_location) != ship_size: 
-                            print('Sorry either one of these coordinates has been taken or one of these coordinates does not exist on the board')
-                            new_start_coor_option = input('Enter 1 if you would like to try a different orientation or 2 if you would like to change your starting coordinate   ')
-                            if new_start_coor_option == '1':
-                                pass
-                            else:
-                                self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
-                        else:
-                            player_ship_instance.place_ship(ship_location, dict_name)
-                            print(player_ship_instance.locations.items())
-                            player_board.player_set_ship(ship_location, ship_letter)
-                            break
-                    else:
-                        print('sorry that is not a valid input please try again')
+        if player_ship_instance.check_start_loc(format_coord):
+            print('Sorry this coordinate is already occupied!')
+            self.ship_input(ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)                              
+        not_placed = True
+        while not_placed == True:                                     
+            orientation = input('''From your coordinate what orientation would you like to place your ship on?\nenter 'up', 'down', 'left' or 'right':   ''')
+            if orientation.lower() == 'up':
+                not_placed = self.ship_orientation(format_coord, Coordinate.coords_up, ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
+            elif orientation.lower() == 'down':
+                not_placed = self.ship_orientation(format_coord, Coordinate.coords_down, ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)       
+            elif orientation.lower() == 'left':
+                not_placed = self.ship_orientation(format_coord, Coordinate.coords_left, ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
+            elif orientation.lower() == 'right':
+                not_placed = self.ship_orientation(format_coord, Coordinate.coords_right, ship_type, ship_size, player_ship_instance, player_board, ship_number, ship_letter, dict_name)
 
     def player_guess(self, computer_ship_instance, computer_board_instance, player_instance):
         player_guess = input('enter a coordinate to shoot at   ')
         format_guess = self.format_input(player_guess)
         if format_guess == None:
             self.player_guess(computer_ship_instance, computer_board_instance, player_instance)
-        else:       
-            if format_guess in self.guesses:
-                print('Sorry you have already guessed this coordinate')
-                self.player_guess(computer_ship_instance, computer_board_instance, player_instance)
-            else:
-                self.guesses.append(format_guess)
-                if computer_ship_instance.does_this_hit(format_guess):
-                    print('you hit!')
-                    computer_board_instance.update_board(format_guess, 'X')
-                    sunk = computer_ship_instance.has_ship_sunk(player_instance)
-                    if sunk:
-                        print(f'You have sunk an enemy {computer_ship_instance.sunk_ship_names[-1]}!')
-                else:
-                    print('you missed!')
-                    computer_board_instance.update_board(format_guess, 'O')
+        if format_guess in self.guesses:
+            print('Sorry you have already guessed this coordinate')
+            self.player_guess(computer_ship_instance, computer_board_instance, player_instance)
+        self.guesses.append(format_guess)
+        if computer_ship_instance.does_this_hit(format_guess):
+            print('you hit!')
+            computer_board_instance.update_board(format_guess, 'X')
+            sunk_ship = computer_ship_instance.has_ship_sunk(player_instance)
+            if sunk_ship:
+                sunk_ship = (sunk_ship[0]).split(' ', 1)
+                print(f'You have sunk an enemy {sunk_ship}!')
+        else:
+            print('you missed!')
+            computer_board_instance.update_board(format_guess, 'O')
